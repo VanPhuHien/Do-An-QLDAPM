45f4db4f73e7affed4cbf6fd797518d85a2749e5
nvk3005 <nguyenvankent3005@gmail.com>
2026-04-07 13:03:36 +0700

    SCRUM-71 Implement chấp nhận / từ chối lời mời, tránh gửi trùng

--- back-end/src/main/java/com/okayji/feed/service/FriendService.java
+++ back-end/src/main/java/com/okayji/feed/service/FriendService.java
@@ -8,4 +8,7 @@ public interface FriendService {
     void createFriendRequest(String fromUserId, String toUserIdOrUsername);
     List<FriendReqResponse> getFriendRequestSent(String userId);
     List<FriendReqResponse> getFriendRequestReceived(String userId);
+    void acceptFriendRequest(String friendRequestId);
+    void deleteFriendRequest(String friendRequestId);
+    void unfriend(String fromUserId, String anotherUserIdOrUsername);
 }
--- back-end/src/main/java/com/okayji/feed/service/impl/FriendServiceImpl.java
+++ back-end/src/main/java/com/okayji/feed/service/impl/FriendServiceImpl.java
@@ -22,6 +22,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
@@ -65,10 +66,60 @@ public class FriendServiceImpl implements FriendService {
 
             // Ping noti to other user
             notificationService.sendNotification(NotificationFactory.friendRequest(friendRequest));
-        } catch (IllegalArgumentException e) {
+        }
+        catch (IllegalArgumentException e) {
             throw new AppException(AppError.FRIEND_YOURSELF);
         }
     }
+
+    @Override
+    @Transactional
+    public void acceptFriendRequest(String friendRequestId) {
+        FriendRequest friendRequest = friendRequestRepository.findById(friendRequestId)
+                .orElseThrow(() -> new AppException(AppError.FRIEND_REQUEST_NOT_FOUND));
+
+        User sender = friendRequest.getSender();
+        User receiver = friendRequest.getReceiver();
+        var pair = PairUser.canonical(sender, receiver);
+
+        friendRepository.save(Friend.builder()
+                .userLow(pair.getLow())
+                .userHigh(pair.getHigh())
+                .build());
+
+        // Delete request after accept
+        friendRequestRepository.delete(friendRequest);
+
+        // Create chat between 2 user
+        chatService.createDirectChat(receiver.getId(), sender.getId());
+
+        // Ping noti to other user
+        notificationService.sendNotification(NotificationFactory.newFriend(sender, receiver));
+    }
+
+    @Override
+    public void deleteFriendRequest(String friendRequestId) {
+        friendRequestRepository.delete(
+                friendRequestRepository.findById(friendRequestId)
+                        .orElseThrow(() -> new AppException(AppError.FRIEND_REQUEST_NOT_FOUND))
+        );
+    }
+
+    @Override
+    public void unfriend(String fromUserId, String anotherUserIdOrUsername) {
+        User fromUser = userRepository.findById(fromUserId)
+                .orElseThrow(() -> new AppException(AppError.USER_NOT_FOUND));
+        User anotherUser = userRepository.findUserByIdOrUsername(anotherUserIdOrUsername, anotherUserIdOrUsername)
+                .orElseThrow(() -> new AppException(AppError.USER_NOT_FOUND));
+
+        var pair = PairUser.canonical(anotherUser, fromUser);
+        Friend friend = friendRepository.findByUserLowAndUserHigh(pair.getLow(), pair.getHigh());
+        if (Objects.isNull(friend))
+            throw new AppException(AppError.NOT_FRIEND);
+
+        friendRepository.delete(friend);
+    }
+
 
     @Override
     public List<FriendReqResponse> getFriendRequestSent(String userId) {
